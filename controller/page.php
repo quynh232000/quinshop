e19fb188ea175ae96644c387d80f3a736b6e605f
quynh232000 <quynh@gmail.com>
2023-11-29 11:02:33 +0700

    qwe

--- controller/page.php
+++ controller/page.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'view/inc/header.php';
+
 
 include_once 'model/product.php';
 include_once 'model/entity.php';
@@ -13,10 +13,11 @@ $cartResult = $classCart->getCartUser();
 
 extract($_REQUEST);
 include_once 'model/category.php';
+include_once 'view/inc/header.php';
+include_once 'view/collection.php';
+include_once 'view/home.php';
+include_once 'view/inc/footer.php';
 if (isset($act)) {
-    include_once 'view/collection.php';
-    include_once 'view/home.php';
-    include_once 'view/inc/footer.php';
     switch ($act) {
         case 'home':
             $allCategory = $cate->getAllCate();
