8958313e9142fc9ff84bff833603ae37b80a8091
quynh232000 <quynh@gmail.com>
2023-11-29 11:11:06 +0700

    12345

--- controller/page.php
+++ controller/page.php
@@ -1,13 +1,13 @@
 <?php
 
 
-include_once 'model/product.php';
+// include_once 'model/product.php';
 include_once 'model/entity.php';
+include_once 'model/category.php';
 include_once 'view/inc/header.php';
 include_once 'view/home.php';
 include_once 'view/inc/footer.php';
 include_once "model/cart.php";
-include_once 'model/category.php';
 $cate = new Category();
 $product = new Product();
 $classCart = new Cart();
